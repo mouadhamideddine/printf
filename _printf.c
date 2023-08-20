11860a8c278303696969efe4d840d9fc871708d4
mouadhamideddine <mouadhamideddine@gmail.com>
2023-08-20 12:00:16 -0700

    2

--- _printf.c
+++ _printf.c
@@ -14,6 +14,50 @@ int _putchar(char c)
 }
 
 /**
+ * _print_number - Prints a number to stdout
+ * @n: The number to print
+ * @base: The base for conversion (e.g., 10 for decimal, 2 for binary)
+ *
+ * Return: The number of digits printed
+ */
+int _print_number(unsigned int n, int base)
+{
+	const char *digits = "0123456789ABCDEF";
+	int len = 0;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
+
+	if (base == 2)
+	{
+		len += _print_number(n / 2, base);
+		_putchar((n % 2) + '0');
+		len++;
+	}
+	else if (base == 10)
+	{
+		if (n / 10)
+			len += _print_number(n / 10, base);
+
+		_putchar((n % 10) + '0');
+		len++;
+	}
+	else if (base == 16)
+	{
+		if (n / 16)
+			len += _print_number(n / 16, base);
+
+		_putchar(digits[n % 16]);
+		len++;
+	}
+
+	return (len);
+}
+
+/**
  * _printf - Produces output according to a format
  * @format: The format string
  *
@@ -52,6 +96,13 @@ int _printf(const char *format, ...)
 				case '%':
 					_putchar('%');
 					len++;
+					break;
+				case 'd':
+				case 'i':
+					len += _print_number(va_arg(args, int), 10);
+					break;
+				case 'b':
+					len += _print_number(va_arg(args, unsigned int), 2);
 					break;
 				default:
 					_putchar('%');
