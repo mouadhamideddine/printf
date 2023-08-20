703f098431ffef8b5dedb0adaeb6013f5b551b7e
mouadhamideddine <mouadhamideddine@gmail.com>
2023-08-20 11:57:33 -0700

    0

--- _printf.c
+++ _printf.c
@@ -1,47 +1,74 @@
-#include <stdio.h>
 #include <stdarg.h>
-#include "main.h"
+#include <unistd.h>
+#include <stdlib.h>
 
+/**
+ * _putchar - Writes a character to stdout
+ * @c: The character to write
+ *
+ * Return: On success, return 1. On error, return -1.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * _printf - Produces output according to a format
+ * @format: The format string
+ *
+ * Return: The number of characters printed (excluding the null byte)
+ */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    va_start(args, format);
+	va_list args;
+	int len = 0;
+	char *str;
 
-    int count = 0;
-    const char *p = format;
+	va_start(args, format);
 
-    while (*p != '\0') {
-        if (*p == '%') {
-            p++;  // Move past the '%'
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+				case 'c':
+					_putchar(va_arg(args, int));
+					len++;
+					break;
+				case 's':
+					str = va_arg(args, char *);
+					if (str == NULL)
+						str = "(null)";
+					while (*str)
+					{
+						_putchar(*str);
+						str++;
+						len++;
+					}
+					break;
+				case '%':
+					_putchar('%');
+					len++;
+					break;
+				default:
+					_putchar('%');
+					_putchar(*format);
+					len += 2;
+					break;
+			}
+		}
+		else
+		{
+			_putchar(*format);
+			len++;
+		}
+		format++;
+	}
 
-            // Handle the conversion specifier
-            switch (*p) {
-                case 'c':
-                    // Fetch the next argument and print it as a character
-                    putchar(va_arg(args, int));
-                    count++;
-                    break;
-                case 's':
-                    // Fetch the next argument and print it as a string
-                    count += printf("%s", va_arg(args, const char *));
-                    break;
-                case '%':
-                    // Print a literal '%'
-                    putchar('%');
-                    count++;
-                    break;
-                default:
-                    // Invalid conversion specifier, skip it
-                    break;
-            }
-        } else {
-            putchar(*p);
-            count++;
-        }
+	va_end(args);
 
-        p++;
-    }
-
-    va_end(args);
-    return count;
+	return (len);
 }
