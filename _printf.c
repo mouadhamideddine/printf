33238c9fda83f6e0963630ea74d5f5aeec5a1fdd
mouadhamideddine <mouadhamideddine@gmail.com>
2023-08-20 12:20:42 -0700

    0

--- _printf.c
+++ _printf.c
@@ -10,7 +10,7 @@
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+    return (write(1, &c, 1));
 }
 
 /**
@@ -20,41 +20,40 @@ int _putchar(char c)
  *
  * Return: The number of digits printed
  */
-int _print_number(unsigned int n, int base)
+int _print_number(int n, int base)
 {
-	const char *digits = "0123456789ABCDEF";
-	int len = 0;
+    const char *digits = "0123456789ABCDEF";
+    int len = 0;
 
-	if (n == 0)
-	{
-		_putchar('0');
-		return (1);
-	}
+    if (n == 0)
+    {
+        _putchar('0');
+        return (1);
+    }
 
-	if (base == 2)
-	{
-		len += _print_number(n / 2, base);
-		_putchar((n % 2) + '0');
-		len++;
-	}
-	else if (base == 10)
-	{
-		if (n / 10)
-			len += _print_number(n / 10, base);
+    if (n < 0)
+    {
+        _putchar('-');
+        len++;
+        n = -n;
+    }
 
-		_putchar((n % 10) + '0');
-		len++;
-	}
-	else if (base == 16)
-	{
-		if (n / 16)
-			len += _print_number(n / 16, base);
+    if (base == 10 || base == 16)
+    {
+        if (n / base)
+            len += _print_number(n / base, base);
 
-		_putchar(digits[n % 16]);
-		len++;
-	}
+        _putchar(digits[n % base]);
+        len++;
+    }
+    else if (base == 2)
+    {
+        len += _print_number(n / 2, base);
+        _putchar((n % 2) + '0');
+        len++;
+    }
 
-	return (len);
+    return (len);
 }
 
 /**
@@ -65,61 +64,59 @@ int _print_number(unsigned int n, int base)
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int len = 0;
-	char *str;
+    va_list args;
+    int len = 0;
 
-	va_start(args, format);
+    va_start(args, format);
 
-	while (*format)
-	{
-		if (*format == '%')
-		{
-			format++;
-			switch (*format)
-			{
-				case 'c':
-					_putchar(va_arg(args, int));
-					len++;
-					break;
-				case 's':
-					str = va_arg(args, char *);
-					if (str == NULL)
-						str = "(null)";
-					while (*str)
-					{
-						_putchar(*str);
-						str++;
-						len++;
-					}
-					break;
-				case '%':
-					_putchar('%');
-					len++;
-					break;
-				case 'd':
-				case 'i':
-					len += _print_number(va_arg(args, int), 10);
-					break;
-				case 'b':
-					len += _print_number(va_arg(args, unsigned int), 2);
-					break;
-				default:
-					_putchar('%');
-					_putchar(*format);
-					len += 2;
-					break;
-			}
-		}
-		else
-		{
-			_putchar(*format);
-			len++;
-		}
-		format++;
-	}
+    while (*format)
+    {
+        if (*format == '%')
+        {
+            format++;
+            switch (*format)
+            {
+            case 'c':
+                _putchar(va_arg(args, int));
+                len++;
+                break;
+            case 's':
+                {
+                    char *str = va_arg(args, char *);
+                    if (str == NULL)
+                        str = "(null)";
+                    while (*str)
+                    {
+                        _putchar(*str);
+                        str++;
+                        len++;
+                    }
+                }
+                break;
+            case '%':
+                _putchar('%');
+                len++;
+                break;
+            case 'd':
+            case 'i':
+                len += _print_number(va_arg(args, int), 10);
+                break;
+            default:
+                _putchar('%');
+                _putchar(*format);
+                len += 2;
+                break;
+            }
+        }
+        else
+        {
+            _putchar(*format);
+            len++;
+        }
+        format++;
+    }
 
-	va_end(args);
+    va_end(args);
 
-	return (len);
+    return (len);
 }
